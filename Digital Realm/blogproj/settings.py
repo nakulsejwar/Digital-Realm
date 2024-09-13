53ef719244ba791bbc867353f214f54875ecfbfe
nakulsejwar <67112941+nakulsejwar@users.noreply.github.com>
2024-09-13 10:14:04 -0700

    Update settings.py

--- Digital Realm/blogproj/settings.py
+++ Digital Realm/blogproj/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
